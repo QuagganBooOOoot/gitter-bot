9dff4f0b9c0d7188613fc38fb7bbbd9a7fd05766
darthmaim <git@darthmaim.de>
2017-03-24 00:04:31 +0100

    link chatlinks to api

--- src/commands/chatlinks.js
+++ src/commands/chatlinks.js
@@ -6,7 +6,21 @@ export function chatlinks(room, message) {
     const rows = [];
 
     uniq(message.text.match(/\[&[a-zA-Z0-9+\/]+=*\]/ig)).forEach(link => {
-        rows.push([link, JSON.stringify(decode(link))]);
+        const data = decode(link);
+
+        switch(data.type) {
+            case 'item':
+            case 'map':
+            case 'skill':
+            case 'trait':
+            case 'recipe':
+            case 'skin':
+            case 'outfit':
+                link = `[${link}](https://api.guildwars2.com/v2/${data.type}s/${data.id})`;
+                break;
+        }
+
+        rows.push([link, JSON.stringify(data)]);
     });
 
     if(rows.length > 0) {
