becea26a85801f8b3122d34d03105ecbc052c262
darthmaim <git@darthmaim.de>
2017-03-30 01:09:19 +0200

    compare achievement progress of multiple users at once

--- src/commands/achievement.js
+++ src/commands/achievement.js
@@ -3,18 +3,24 @@ import { getApiKey, gw2 } from '../gw2'
 // shows achievement progress
 export function achievement(room, message) {
     const achievementId = message.text.split(' ', 3)[1];
-    const userId = message.mentions[0] ? message.mentions[0].userId : message.fromUser.id;
-    const userName = message.mentions[0] ? message.mentions[0].screenName : message.fromUser.username;
+
+    const users = [{ id: message.fromUser.id, name: message.fromUser.username }];
+    message.mentions.forEach(user => users.push({ id: user.userId, name: user.screenName }));
 
     gw2.achievements().get(achievementId).then(achievement => {
-        getApiKey(userId).then(apiKey => {
-            gw2.authenticate(apiKey).account().achievements().get(achievementId).then(progress => {
-                room.send(`**${achievement.name}**  \n${achievement.description}\n\n@${userName}'s progress: ${progress.current}/${progress.max}`);
-            }).catch(error => {
-                room.send(`**${achievement.name}**  \n${achievement.description}\n\n*No progress for @${userName} (${error})*`)
-            })
-        }).catch(error => {
-            room.send(`**${achievement.name}**  \n${achievement.description}`)
+        Promise.all(
+            users.map(user => getApiKey(user.id)
+                .then(apiKey => gw2.authenticate(apiKey).account().achievements().get(achievementId))
+                .then(progress => ({ user, progress }))
+                .catch(error => ({ user })))
+        ).then(users => {
+            room.send(
+                `**${achievement.name}**  \n${achievement.description}\n\n`
+                + users.map(user =>
+                    user.progress
+                        ? `@${user.user.name}'s progress: ${user.progress.current}/${user.progress.max}`
+                        : `@${user.user.name}'s progress: Missing API key or wrong permissions.`
+                ).join('\n  '));
         });
     }).catch(error => room.send(`Oah! Quaggan couldn't connect to the GW2 Api (${error}).`));
 }
