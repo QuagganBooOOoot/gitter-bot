1a2f6123c3cd4e32d06b2bd8d4735c7bd88859a8
darthmaim <git@darthmaim.de>
2017-03-30 01:52:33 +0200

    add !achievement to !help

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,6 +5,7 @@ export function help(room, message) {
     room.send(markdownTable(['Command', 'Description'], [
         ['`!auth`', 'Returns your account name.'],
         ['`!auth <API-KEY>`', 'Sets your API key.'],
+        ['`!achievement <ID>`', 'Shows your achievement progress and compares it with all mentioned users.'],
         ['`!rank`', 'Returns your WvW rank.'],
     ])+'\n\nRequest more features in [Quaggans repository](https://github.com/QuagganBooOOoot/gitter-bot/issues).');
 }
